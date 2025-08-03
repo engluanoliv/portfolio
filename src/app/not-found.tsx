e968a2290cd2992d97444ac68011476e433bb347
engluanoliv <engluanoliv@gmail.com>
2025-08-02 22:15:36 -0300

    feat: initial commit with some pages and biome installed

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <h2>Page Not Found</h2>
+      <p>Could not find requested resources</p>
+    </div>
+  );
+}
---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,8 @@
+type ProjectIdPageProps = {
+  params: Promise<{ projectId: string }>;
+};
+
+export default async function ProjectIdPage({ params }: ProjectIdPageProps) {
+  return <></>;
+}
+ 
